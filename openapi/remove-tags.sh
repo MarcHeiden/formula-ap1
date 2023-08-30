dfc50706d364a4ceec79ff234a6bbaa5d136e284
MarcHeiden <74560465+MarcHeiden@users.noreply.github.com>
2023-08-31 00:49:26 +0200

    ci: add workflow to bundle and deploy openapi spec (#13)

---
+++ openapi/remove-tags.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Check if filename parameter was specified
+if [ $# -eq 0 ]; then
+    echo "Filename parameter was not specified."
+    echo "Exit with code 1."
+    exit 1
+fi
+
+patterns=("Controller" "TeamsOfSeasons" "DriversOfRaces")
+filename="$1"
+
+for pattern in "${patterns[@]}"; do
+    sed -i "/$pattern/d" "$filename"
+done
+
+echo "Removed tags from '$filename'."
