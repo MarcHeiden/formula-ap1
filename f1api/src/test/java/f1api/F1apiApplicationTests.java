78aac61c1c9e14343cb7c7d4cfab5c8158b2b136
MarcHeiden <74560465+MarcHeiden@users.noreply.github.com>
2023-09-01 17:22:47 +0200

    feat(api): initialize spring boot project (#20)

---
+++ f1api/src/test/java/f1api/F1apiApplicationTests.java
@@ -0,0 +1,11 @@
+package f1api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class F1apiApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
