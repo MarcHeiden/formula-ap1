86746803751d46d4c1e73e3e69bbfed4caca2b8b
MarcHeiden <74560465+MarcHeiden@users.noreply.github.com>
2023-09-10 17:06:27 +0200

    feat(api): add api property is null exception

---
+++ f1api/src/main/java/f1api/exception/ApiPropertyIsNullException.java
@@ -0,0 +1,18 @@
+package f1api.exception;
+
+import java.util.List;
+import org.springframework.http.HttpStatus;
+
+public class ApiPropertyIsNullException extends ApiException {
+    public ApiPropertyIsNullException(String message) {
+        super(message, HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    public static ApiPropertyIsNullException of(List<String> properties) {
+        StringBuilder message = new StringBuilder();
+        properties.forEach(property -> message.append(property).append(" or "));
+        int startIndex = message.lastIndexOf("or");
+        message.delete(startIndex, startIndex + 3).append("must not be null.");
+        return new ApiPropertyIsNullException(message.toString());
+    }
+}
