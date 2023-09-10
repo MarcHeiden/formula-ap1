c34841cb8a691fe5406d6aef431d81e41fcf39a3
MarcHeiden <74560465+MarcHeiden@users.noreply.github.com>
2023-09-10 17:05:15 +0200

    feat(api): add factory method for api instance already exists exception

--- f1api/src/main/java/f1api/exception/ApiInstanceAlreadyExistsException.java
+++ f1api/src/main/java/f1api/exception/ApiInstanceAlreadyExistsException.java
@@ -1,5 +1,6 @@
 package f1api.exception;
 
+import java.util.Map;
 import org.springframework.http.HttpStatus;
 
 public class ApiInstanceAlreadyExistsException extends ApiException {
@@ -11,5 +12,14 @@ public class ApiInstanceAlreadyExistsException extends ApiException {
     public static ApiInstanceAlreadyExistsException of(String instance, String property, String propertyValue) {
         String message = instance + " with the " + property + " '" + propertyValue + "' already exists.";
         return new ApiInstanceAlreadyExistsException(message);
+    }
+
+    public static ApiInstanceAlreadyExistsException of(String instance, Map<String, String> properties) {
+        StringBuilder message = new StringBuilder();
+        message.append(instance).append(" with ");
+        properties.forEach((property, propertyValue) ->
+                message.append(property).append(" '").append(propertyValue).append("', "));
+        message.deleteCharAt(message.lastIndexOf(",")).append("already exists.");
+        return new ApiInstanceAlreadyExistsException(message.toString());
     }
 }
