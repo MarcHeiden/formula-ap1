17415b1d015ec71efb176575d9d8cbb7df448329
MarcHeiden <74560465+MarcHeiden@users.noreply.github.com>
2023-09-12 21:36:55 +0200

    feat(api): add helper function/variable to driver dto

--- f1api/src/main/java/f1api/driver/application/DriverDTO.java
+++ f1api/src/main/java/f1api/driver/application/DriverDTO.java
@@ -1,10 +1,13 @@
 package f1api.driver.application;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import f1api.validation.OnCreate;
 import jakarta.validation.constraints.NotNull;
+import java.util.List;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @NoArgsConstructor
@@ -18,4 +21,13 @@ public class DriverDTO {
 
     @NotNull(groups = OnCreate.class)
     private String lastName;
+
+    @JsonIgnore
+    public Boolean isEmpty() {
+        return firstName == null && lastName == null;
+    }
+
+    @Getter
+    @JsonIgnore
+    private static final List<String> notNullProperties = List.of("firstName", "lastName");
 }
